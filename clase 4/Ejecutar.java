1f4006e2f09506832461656aeca036bd9c0727a1
Leonardo-909 <leonardoangel@estudiante.uniajc.edu.co>
2025-02-25 17:28:37 +0000

    matrices

---
+++ clase 4/Ejecutar.java
@@ -0,0 +1,13 @@
+public class Ejecutar {
+
+    public static void main(String[] args) {
+
+        Ejercicio20 objMatriz = new Ejercicio20();
+
+        int[][] m = objMatriz.llenarMatriz(10, 10);
+
+        System.out.println(objMatriz.mostrarMatriz(m));
+
+        objMatriz.lle
+    }
+}
