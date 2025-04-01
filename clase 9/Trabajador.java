cb9af3203f54c5473a510a37992ed7b902739fea
Leonardo-909 <leonardoangel@estudiante.uniajc.edu.co>
2025-04-01 16:28:24 +0000

    creacion de los metodon de nombre salario y cedula

--- clase 9/Trabajador.java
+++ clase 9/Trabajador.java
@@ -1,15 +1,36 @@
 public class Trabajador {
-
+ 
     private String cedula;
     private String nombre;
     private double salario;
-
-    // Constructor
-    public Trabajador(String cedula, String nombre, double salario) {
+   
+    public Trabajador(String cedula, String nombre, double salario){
         this.cedula = cedula;
         this.nombre = nombre;
         this.salario = salario;
     }
+ 
+    public String getCedula(){
+        return cedula;
+    }
+ 
+    public String getNombre(){
+        return nombre;
+    }
+ 
+    public double getSalario(){
+        return salario;
+    }
 
+    public String toString() {
+        return "Trabajador{" +
+                "cedula='" + cedula + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", salario=" + salario +
+                '}';
+    }
     
+    public double pagar(double incremento) {
+        return salario +( salario * incremento );
+    }
 }
